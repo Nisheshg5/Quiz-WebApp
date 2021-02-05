c624efad94d90146e1e56115bf01ea02011fa53f
Nishesh Garg <Nisheshg5@gmail.com>
2021-02-05 13:23:17 +0530

    Added database

--- quiz_project/settings.py
+++ quiz_project/settings.py
@@ -77,8 +77,13 @@ WSGI_APPLICATION = "quiz_project.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "quiz",
+        "USER": "adminsql@quiz-mysql-db",
+        "PASSWORD": "Quiz@123",
+        "HOST": "quiz-mysql-db.mysql.database.azure.com",
+        "PORT": "3306",
+        "OPTIONS": {"init_command": "SET sql_mode='STRICT_TRANS_TABLES'"},
     }
 }
 
