9aea12ecd9f60505bc3064519d992463419a35c2
Nishesh Garg <Nisheshg5@gmail.com>
2021-02-06 21:42:40 +0530

    Made quiz password form

--- quiz_app/forms.py
+++ quiz_app/forms.py
@@ -1,19 +1,36 @@
 from django import forms
 from django.forms import fields
 
-from .models import Quiz, Account
+from .models import Account, Quiz
 
 
 class QuizForm(forms.Form):
     quiz_id = forms.CharField(
         max_length=36,
-        widget=forms.TextInput(attrs={"class": "form-control form-control-lg", "placeholder": "Enter Quiz Code"}),
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control form-control-lg",
+                "placeholder": "Enter Quiz Code",
+            }
+        ),
     )
     # class Meta:
     #     model = Quiz
     #     fields = [
     #         'quiz_id'
     #     ]
+
+
+class QuizPasswordForm(forms.ModelForm):
+    password = forms.CharField(
+        max_length=32,
+        widget=forms.PasswordInput(attrs={"autocomplete": "new-password"}),
+    )
+
+    class Meta:
+        model = Quiz
+        fields = ["password"]
+
 
 # Class LoginForm(forms.Form):
 #     email = forms.EmailField(
