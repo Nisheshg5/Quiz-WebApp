1746bd1d6583688b9dd25d17656d57973598d991
Nishesh Garg <Nisheshg5@gmail.com>
2021-02-11 23:15:40 +0530

    Added question bank admin

--- quiz_app/admin.py
+++ quiz_app/admin.py
@@ -1,8 +1,9 @@
+from django import forms
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
 from .forms import SignUpForm
-from .models import Account
+from .models import Account, Question_bank, Quiz
 
 
 class AccountAdmin(UserAdmin):
@@ -32,4 +33,27 @@ class AccountAdmin(UserAdmin):
     fieldsets = ()
 
 
+class QuestionBankAdmin(admin.ModelAdmin):
+    list_display = (
+        "question",
+        "choice_1",
+        "choice_2",
+        "choice_3",
+        "choice_4",
+        "choice_5",
+        "correct",
+        "isShuffle",
+        "tag",
+        "level",
+    )
+    search_fields = ("question",)
+    list_filter = (
+        "tag",
+        "level",
+    )
+    fieldsets = ()
+
+
 admin.site.register(Account, AccountAdmin)
+admin.site.register(Quiz)
+admin.site.register(Question_bank, QuestionBankAdmin)
