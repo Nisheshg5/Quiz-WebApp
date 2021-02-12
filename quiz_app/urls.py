8c34cce7e50762b948170ab3da268f11b9ac2666
TheInfinity007 <ramchandra71999@gmail.com>
2021-02-12 19:56:21 +0530

    add styles to instruction page

--- quiz_app/urls.py
+++ quiz_app/urls.py
@@ -10,9 +10,7 @@ urlpatterns = [
     path("quiz/upcoming/<quiz_id>", views.quiz_upcoming, name="quiz_upcoming"),
     path("quiz/started/<quiz_id>", views.quiz_started, name="quiz_started"),
     path("quiz/ended/<quiz_id>", views.quiz_ended, name="quiz_ended"),
-    path(
-        "quiz/instructions/<quiz_id>", views.quiz_instructions, name="quiz_instructions"
-    ),
+    path("quiz/instructions/<quiz_id>", views.quiz_instructions, name="quiz_instructions"),
     path("hello/<name>", views.hello_there, name="hello_there"),
     path("about/", views.about, name="about"),
     path("contact/", views.contact, name="contact"),
