f07d5ced85015abe83197132cc79d158eb61e121
Nishesh Garg <Nisheshg5@gmail.com>
2021-02-18 03:52:06 +0530

    Added has ended propery to quizTaker

--- quiz_app/models.py
+++ quiz_app/models.py
@@ -200,6 +200,13 @@ class QuizTakers(models.Model):
             self.started + timedelta(minutes=self.quiz.duration) - timezone.now()
         ).total_seconds()
 
+    @property
+    def has_ended(self):
+        time = self.started + timedelta(minutes=self.quiz.duration) - timezone.now()
+        if time.total_seconds() <= 0 and not self.completed:
+            self.completed = self.started + timedelta(minutes=self.quiz.duration)
+        return time.total_seconds() <= 0
+
     class Meta:
         db_table = "QuizTaker"
         app_label = "quiz_app"
