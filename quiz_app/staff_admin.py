08488b6b9cb3bf4c498c9decca6e38032ab54e37
Nishesh Garg <Nisheshg5@gmail.com>
2021-03-08 20:55:07 +0530

    Add admin of account and question bank for staff admin

--- quiz_app/staff_admin.py
+++ quiz_app/staff_admin.py
@@ -1,17 +1,27 @@
 import json
 from collections import Counter
 
-from django.contrib import admin
-from django.contrib.admin import AdminSite
+from django.contrib import admin, messages
+from django.contrib.admin import AdminSite, SimpleListFilter
+from django.contrib.auth.admin import UserAdmin
+from django.core.exceptions import ValidationError
 from django.db import models
-from django.db.models import Sum
+from django.db.models import Count, F, Sum
 from django.forms import Textarea
+from django.forms.models import model_to_dict
 from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, redirect, render
+from django.template import RequestContext
 from django.template.response import TemplateResponse
 from django.urls import path, reverse
+from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext_lazy as _
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+from import_export.fields import Field
+from import_export.formats import base_formats
 
-from .forms import QuizAddFormStaff
+from .forms import QuizAddForm, QuizAddFormStaff, SignUpForm
 from .models import Account, Question, Question_bank, Quiz, QuizTakers, Response
 
 
@@ -22,6 +32,108 @@ class StaffAdminSite(AdminSite):
     site_header = "Staff Admin"
     site_title = "Staff Admin Portal"
     index_title = "Welcome"
+
+
+class AccountAdmin(UserAdmin):
+    class EmptyQuizIDFilter(SimpleListFilter):
+        title = _("Empty Filter For Quiz")
+        parameter_name = "quizid"
+
+        def lookups(self, request, model_admin):
+            return ()
+
+    def get_action_choices(self, request):
+        choices = super(AccountAdmin, self).get_action_choices(request)
+        choices.pop(0)
+        choices.reverse()
+        try:
+            quiz_id = request.GET.get("quizid", None)
+            if not quiz_id:
+                raise Quiz.DoesNotExist()
+            Quiz.objects.get(pk=quiz_id)
+        except (Quiz.DoesNotExist, ValidationError):
+            choices.pop(0)
+        return choices
+
+    def assign_users(self, request, queryset):
+        quiz_id = request.GET.get("quizid", "")
+        if not quiz_id:
+            messages.error(
+                request, mark_safe("No Quiz Entered<br>Please Select A Quiz First")
+            )
+            return redirect(request.get_full_path())
+
+        try:
+            quiz = Quiz.objects.prefetch_related(
+                "quiztakers_set", "quiztakers_set__user"
+            ).get(pk=quiz_id)
+        except ValidationError:
+            messages.error(request, mark_safe("Invalid Quiz Id Found"))
+            return redirect(request.get_full_path())
+
+        if not quiz:
+            messages.error(request, mark_safe("No Quiz Found For Given ID"))
+            return redirect(request.get_full_path())
+
+        if "apply" in request.POST:
+            if request.POST.get("apply", "") == "Cancel":
+                return redirect(request.get_full_path())
+            quizTakers = []
+            for user in queryset:
+                quizTaker = QuizTakers(quiz=quiz, user=user)
+                quizTakers.append(quizTaker)
+            QuizTakers.objects.bulk_create(quizTakers, ignore_conflicts=True)
+            return redirect(request.get_full_path())
+
+        context = {
+            "quiz": quiz,
+            "users": queryset,
+        }
+        return render(request, "staff_admin/user_confirmation.html", context=context)
+
+    assign_users.short_description = "Assign Students To Test"
+
+    add_form = SignUpForm
+
+    list_display = (
+        "full_name",
+        "email",
+        "date_joined",
+        "last_login",
+        "timeZone",
+        "is_active",
+        "is_admin",
+        "is_staff",
+    )
+    search_fields = (
+        "full_name",
+        "email",
+    )
+    ordering = (
+        "full_name",
+        "email",
+    )
+
+    readonly_fields = ("id", "date_joined", "last_login")
+
+    actions = ["assign_users"]
+    filter_horizontal = ()
+    list_filter = (EmptyQuizIDFilter,)
+    fieldsets = (
+        (None, {"fields": ("email", "password"),}),
+        (_("Personal info"), {"fields": ("full_name", "timeZone")}),
+        (_("Permissions"), {"fields": ("is_active", "is_staff", "is_superuser",),}),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("full_name", "email", "password1", "password2"),
+            },
+        ),
+    )
 
 
 class QuizAdmin(admin.ModelAdmin):
@@ -107,7 +219,7 @@ class QuizAdmin(admin.ModelAdmin):
             has_view_permission=True,
             has_editable_inline_admin_formsets=True,
         )
-        return TemplateResponse(request, "admin/quiz_report.html", context)
+        return TemplateResponse(request, "staff_admin/quiz_report.html", context)
 
     def get_action_choices(self, request):
         choices = super(QuizAdmin, self).get_action_choices(request)
@@ -144,10 +256,129 @@ class QuizAdmin(admin.ModelAdmin):
         QuestionAdmin,
     ]
     form = QuizAddFormStaff
-    change_form_template = "admin/quiz_change_form.html"
+    change_form_template = "staff_admin/quiz_change_form.html"
+
+
+class Question_bank_admin(ImportExportModelAdmin):
+    class Question_bank_resource(resources.ModelResource):
+        id = Field(attribute="id")
+        title = Field(attribute="title", column_name="Question Statement")
+        choice_1 = Field(attribute="choice_1", column_name="Option 1")
+        choice_2 = Field(attribute="choice_2", column_name="Option 2")
+        choice_3 = Field(attribute="choice_3", column_name="Option 3")
+        choice_4 = Field(attribute="choice_4", column_name="Option 4")
+        choice_5 = Field(attribute="choice_5", column_name="Option 5")
+        correct = Field(attribute="correct", column_name="Correct Answer-1")
+        marks = Field(attribute="marks", column_name="Marks", default=1)
+        tag = Field(attribute="tag", column_name="Tag")
+        isShuffle = Field(attribute="isShuffle", column_name="isShuffle")
+        level = Field(attribute="level", column_name="Level", default="easy")
+
+        class Meta:
+            model = Question_bank
+
+    class EmptyQuizIDFilter(SimpleListFilter):
+        title = _("Empty Filter For Quiz")
+        parameter_name = "quizid"
+
+        def lookups(self, request, model_admin):
+            return ()
+
+    def get_import_formats(self):
+        formats = (
+            base_formats.XLS,
+            base_formats.XLSX,
+        )
+        return [f for f in formats if f().can_import()]
+
+    def get_action_choices(self, request):
+        choices = super(Question_bank_admin, self).get_action_choices(request)
+        choices.pop(0)
+        choices.reverse()
+        try:
+            quiz_id = request.GET.get("quizid", None)
+            if not quiz_id:
+                raise Quiz.DoesNotExist()
+            quiz = Quiz.objects.get(pk=quiz_id)
+        except (Quiz.DoesNotExist, ValidationError):
+            choices.pop(0)
+        return choices
+
+    def add_questions_to_quiz(self, request, queryset):
+        quiz_id = request.GET.get("quizid", "")
+        if not quiz_id:
+            messages.error(
+                request, mark_safe("No Quiz Entered<br>Please Select A Quiz First")
+            )
+            return redirect(request.get_full_path())
+
+        try:
+            quiz = Quiz.objects.filter(pk=quiz_id).first()
+        except ValidationError:
+            messages.error(request, mark_safe("Invalid Quiz Id Found"))
+            return redirect(request.get_full_path())
+
+        if not quiz:
+            messages.error(request, mark_safe("No Quiz Found For Given ID"))
+            return redirect(request.get_full_path())
+
+        if "apply" in request.POST:
+            if request.POST.get("apply", "") == "Cancel":
+                return redirect(request.get_full_path())
+
+            questions = []
+            for questionDict in queryset:
+                question = Question(
+                    quiz=quiz,
+                    **model_to_dict(questionDict, exclude=["id", "tag", "level"]),
+                )
+                questions.append(question)
+            Question.objects.bulk_create(questions)
+            return redirect(request.get_full_path())
+
+        context = {
+            "question_bank": queryset,
+            "quiz": quiz,
+        }
+        return render(
+            request, "staff_admin/question_bank_confirmation.html", context=context
+        )
+
+    add_questions_to_quiz.short_description = "Add Questions To Quiz"
+
+    list_display = (
+        "title",
+        "choice_1",
+        "choice_2",
+        "choice_3",
+        "choice_4",
+        "choice_5",
+        "correct",
+        "marks",
+        "isShuffle",
+        "tag",
+        "level",
+        "created_at",
+    )
+
+    search_fields = ("title",)
+    list_filter = (
+        "tag",
+        "level",
+        EmptyQuizIDFilter,
+    )
+    readonly_fields = ("created_at",)
+    lookup_fields = [
+        "quiz_id",
+    ]
+    fieldsets = ()
+    actions = ["add_questions_to_quiz"]
+    resource_class = Question_bank_resource
+    change_list_template = "staff_admin/question_bank_list.html"
 
 
 staff_admin_site = StaffAdminSite(name="staff_admin")
 staff_admin_site.enable_nav_sidebar = False
-
+staff_admin_site.register(Account, AccountAdmin)
 staff_admin_site.register(Quiz, QuizAdmin)
+staff_admin_site.register(Question_bank, Question_bank_admin)
