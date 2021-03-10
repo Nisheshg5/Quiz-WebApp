e9c4e712c70f59026d86ec4ca19749991193ea25
TheInfinity007 <ramchandra71999@gmail.com>
2021-03-10 20:35:29 +0530

    crated directory for the face api

---
+++ quiz_app/static/quiz_app/face_detection/script.js
@@ -0,0 +1,50 @@
+const video = document.getElementById("video");
+
+
+// LOAD ALL MODELS ASYNCHRONOUSLY
+// timyFaceDetector - make it fast
+
+Promise.all([
+	faceapi.nets.tinyFaceDetector.loadFromUri('/models')
+]).then(startVideo)
+
+ 
+function startVideo(){
+	navigator.mediaDevices.getUserMedia({ video: {}})
+		.then((stream) => {
+				// console the result
+				console.log("Camera Found");
+
+				// use the stream
+				video.srcObject = stream;				
+		})
+		.catch((err) => {
+			// handle the error
+				console.error(err.name + ": " + err.message);
+		})
+}
+
+video.addEventListener('playing', () => {
+
+	console.log("Video Start playing");
+	const canvas = faceapi.createCanvasFromMedia(video);
+	document.body.append(canvas);
+	const displaySize = { width: video.width, height: video.height};
+	console.log(displaySize);
+	faceapi.matchDimensions(canvas, displaySize);
+
+	setInterval(async () => {
+		const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+		const resizedDetections = faceapi.resizeResults(detections, displaySize);
+		canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+		faceapi.draw.drawDetections(canvas, resizedDetections);
+		console.log(detections);
+		if(detections.length > 1){
+			console.error("Multiple people detected");
+		}else if(detections.length == 1){
+			console.log("User Found");
+		}
+		console.log(detections.length)
+	}, 1000);
+
+})
